774a837d5bff99a5f050be38be310fc9ce1be4b4
404 <113860914+TrafStaf@users.noreply.github.com>
2023-07-02 11:14:29 +0000

    get additional info for admin

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const adminHost='localhost:3000';
+
 const log = console.log;
 
 // Включаємо Express.js, популярний фреймворк для веб-розробки.
@@ -24,8 +26,8 @@ const path = require('path')
 app.use('/static', express.static(path.join(__dirname, 'public')))
 
 // Запускаємо сервер на попередньо встановленому порту.
+log('Server listening at port %d', port);
 server.listen(port, () => {
-    log('Server listening at port %d', port);
 });
 
 // Встановлюємо обробник маршруту "/" для GET запитів. Коли користувач відкриває сайт, ми надсилаємо йому файл index.html.
@@ -49,23 +51,37 @@ function getConnectedSockets() {
 }
 
 //clear data about users
-function getConnectedUsers() {
+function getConnectedUsers(socket) {
     const socketList = getConnectedSockets();
-    const userList = socketList.map((double) => ({
-        id: double[0],
-        username: double[1].username
-    }))
+    const userList = socketList.map((double) => {
+        const obj = {
+            id: double[0],
+            username: double[1].username
+        };
+        const host = socket.handshake.headers.host;
+        if(host==adminHost) obj.host = host;
+        return obj
+    })
     return userList
 }
 
 // for connection, disconnect  events
 function changeConnections(socket) {
-    io.emit('fresh-users-list', getConnectedUsers())
+    io.emit('fresh-users-list', getConnectedUsers(socket))
 }
 // Обробник події з'єднання нового клієнта через Socket.IO
 io.on('connection', (socket) => {
-    //log('connect.....', socket.id);
+    
+    const host = socket.handshake.headers.host;
+    log('connect.....', socket.id, host);
+
+    //console.log(clientIpAddress);
+    //var clientIpAddress = socket.request.connection.remoteAddress;
+
+
     socket.username = 'Anonymous';
+    socket.status='guest';
+    if (host==adminHost)socket.status='admin';
     connectedSockets[socket.id] = socket; // add socket(user)to object // connectedSockets.45jklg6hw45jklg6 = {Soket}
     changeConnections(socket)
 
@@ -73,26 +89,26 @@ io.on('connection', (socket) => {
     if (!chatHistory.main) chatHistory.main = []
     io.to('main').emit('refresh-chat-list', chatHistory['main'])
 
+    //log('Користувач від’єднався, ID сокета: ' + socket.id);
     socket.on('disconnect', () => {
-        //log('Користувач від’єднався, ID сокета: ' + socket.id);
         delete connectedSockets[socket.id]; // Очищення сокета зі списку
         changeConnections(socket)
     });
 
     // При отриманні повідомлення з каналу "chanel1", вивести дані у консоль.
+    // log(data);
     socket.on('chanel1', (data) => {
-        log(data);
         socket.emit('chanel1', 'hello from server')
     });
 
+    //log(username);
     socket.on('set-username', (username) => {
-        //log(username);
         connectedSockets[socket.id].username = username;
-        io.emit('fresh-users-list', getConnectedUsers())
+        io.emit('fresh-users-list', getConnectedUsers(socket))
     });
 
+    // log(message);
     socket.on('message', (message) => {
-        log(message);
         socket.rooms.forEach(r => {
             if (!chatHistory[r]) chatHistory[r] = [];
             chatHistory[r].push({
@@ -105,13 +121,13 @@ io.on('connection', (socket) => {
         // io.emit('refresh-chat-list', chatHistory)
     });
 
+    // log(room);
     socket.on('set-room', (room) => {
-        log(room);
         socket.rooms.forEach(r => {
             socket.leave(r);
         });
+        // log('room', socket.rooms)
         socket.join(room);
-        log('room', socket.rooms)
         socket.rooms.forEach(r => {
             if (!chatHistory[r]) chatHistory[r] = [];
             io.to(r).emit('refresh-chat-list', chatHistory[r])
